e19cb4fff00e744e50848d609c6bbbce27bc00ab
Ty Klayum <ty.klayum@dacis.com>
2025-04-28 10:47:44 -0500

    L2 start finished

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+
+const product = ref('Socks')
+const description = ref('These are either blue or green socks :)')
+</script>
 
 <template>
   <div class="product-display">
     <div class="product-container">
       <div class="product-info">
-        <h1>Socks</h1>
+        <h1>{{ product }}</h1>
+          <p>{{ description }}</p>
       </div>
     </div>
   </div>
